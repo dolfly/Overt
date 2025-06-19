4840ee2d08aad6c498d229d5054ab6610f0b417c
liuxingzuo <liuxingzuo@outlook.com>
2025-06-19 20:15:52 +0800

    update

---
+++ app/src/main/cpp/package_info.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lxz on 2025/6/16.
+//
+
+#include "package_info.h"
---
+++ app/src/main/cpp/package_info.h
@@ -0,0 +1,16 @@
+//
+// Created by lxz on 2025/6/16.
+//
+
+#ifndef OVERT_PACKAGE_INFO_H
+#define OVERT_PACKAGE_INFO_H
+
+
+
+class package_info {
+
+};
+
+
+
+#endif //OVERT_PACKAGE_INFO_H
